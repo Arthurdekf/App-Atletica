2090016dfeacc0ea9abc0d8454a57323597fd6d8
arthurdekf <arthurfedeli@icloud.com>
2023-10-04 23:06:03 -0300

    Novas Telas e Prototipos Java

--- app/src/main/java/com/example/appatletica/RegisterActivity.java
+++ app/src/main/java/com/example/appatletica/RegisterActivity.java
@@ -1,14 +1,16 @@
 package com.example.appatletica;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.appatletica.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        setContentView(R.layout.login_register);
+
     }
 }
