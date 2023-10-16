128dec1bf548c6f807b70838c9e900c17e7d374e
arthurdekf <arthurfedeli@icloud.com>
2023-10-15 21:24:46 -0300

    Novas Telas e correções

--- app/src/main/java/com/example/appatletica/LoginActivity.java
+++ app/src/main/java/com/example/appatletica/LoginActivity.java
@@ -1,25 +1,6 @@
-package com.example.appatletica;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+public class LoginActivity extends AppCompatActivity {
 
-        TextView linkRegistrar = findViewById(R.id.linkRegistrar);
-        linkRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Inicia a atividade de registro
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+
 }
